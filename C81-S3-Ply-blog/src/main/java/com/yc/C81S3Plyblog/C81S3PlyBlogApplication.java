625d61d9deb8f11b7f0468dd97474daaaf91ffae
6606 <306529917@qq.com>
2020-09-13 19:20:54 +0800

    博客工程初始化提交

---
+++ C81-S3-Ply-blog/src/main/java/com/yc/C81S3Plyblog/C81S3PlyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C81S3Plyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C81S3PlyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C81S3PlyBlogApplication.class, args);
+	}
+
+}
---
+++ C81-S3-Ply-blog/src/test/java/com/yc/C81S3Plyblog/C81S3PlyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C81S3Plyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C81S3PlyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
