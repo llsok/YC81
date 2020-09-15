e0a027642ac3a535e70066f54b9028e4a8182df3
liaoyan <liaoyan@chinasofti.com>
2020-09-15 20:02:17 +0800

    分页查询

--- C81-S3-Ply-blog/src/main/java/com/yc/C81S3Plyblog/web/IndexAction.java
+++ C81-S3-Ply-blog/src/main/java/com/yc/C81S3Plyblog/web/IndexAction.java
@@ -7,7 +7,9 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.github.pagehelper.PageHelper;
 import com.yc.C81S3Plyblog.bean.Article;
 import com.yc.C81S3Plyblog.dao.ArticleMapper;
 import com.yc.C81S3Plyblog.dao.CategoryMapper;
@@ -18,15 +20,17 @@ public class IndexAction {
 
 	@Resource
 	private ArticleMapper am;
-	
+
 	@Resource
 	private CategoryMapper cm;
 
 	@GetMapping(path = { "index", "index.html", "/" })
 	// SpringMVC 使用一个 Model 对象传递数据给 页面, Model 通过方法参数注入进来
-	public String index(Model m) {
+	public String index(Model m, @RequestParam(defaultValue = "1") int page) {
 		List<Article> newArticles = am.selectNewArticle();
 		// 将查询出的数据添加到 model 中 发送给页面
+		// 分页查询设置必须在查询方法执行前设定
+		PageHelper.startPage(page, 5);
 		m.addAttribute("newArticles", newArticles);
 		m.addAttribute("categorys", cm.selectAll());
 		return "index";
